6bd0084014e627169a71938b652db8942ec88ff5
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-06-20 23:37:02 -0600

    feat: create ip regex pattern

---
+++ services/core-service/src/utils/ip-regex.ts
@@ -0,0 +1,3 @@
+// stole this off of stackoverflow; cba to figure one out for myself
+// https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp#answer-36760050
+export const ipRegex = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/;
