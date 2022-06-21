99beaf63257d7638d3f69c2fb2159c44c04a3ed3
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-06-20 23:41:50 -0600

    feat: write tests for geoip2 service

--- services/core-service/src/routes/geo2lite/service.test.ts
+++ services/core-service/src/routes/geo2lite/service.test.ts
@@ -1,3 +1,169 @@
+import Service, { Geo2LiteServiceError } from './service';
+
 describe('Geo2Lite Service', () => {
-  it('stub', () => {});
+  it('returns proper address details', async () => {
+    const mockedCity = 'Everywhere, no where..';
+    const mockedCountry = 'There are no countries here';
+    const mockedPostal = 'Postman will have a hard time finding it';
+    const mockedAccuracyRadius = Number.POSITIVE_INFINITY;
+    const mockedTimeZone = 'Just another dimension here travels';
+
+    const repo = {
+      getAddressByIp: jest.fn().mockResolvedValueOnce({
+        city: mockedCity,
+        country: mockedCountry,
+        postal: mockedPostal,
+        location: {
+          accuracyRadius: mockedAccuracyRadius,
+          timeZone: mockedTimeZone,
+        },
+      }),
+    };
+
+    const service = new Service(repo as any);
+
+    const addressDetails = await service.findAddressByIp('0.0.0.0');
+
+    expect(addressDetails.city).toBe(mockedCity);
+    expect(addressDetails.country).toBe(mockedCountry);
+    expect(addressDetails.postal).toBe(mockedPostal);
+    expect(addressDetails.accuracyRadius).toBe(mockedAccuracyRadius);
+    expect(addressDetails.timeZone).toBe(mockedTimeZone);
+  });
+
+  it('throws when no city is found', async () => {
+    const mockedCountry = 'There are no countries here';
+    const mockedPostal = 'Postman will have a hard time finding it';
+    const mockedAccuracyRadius = Number.POSITIVE_INFINITY;
+    const mockedTimeZone = 'Just another dimension here travels';
+
+    const repo = {
+      getAddressByIp: jest.fn().mockResolvedValueOnce({
+        country: mockedCountry,
+        postal: mockedPostal,
+        location: {
+          accuracyRadius: mockedAccuracyRadius,
+          timeZone: mockedTimeZone,
+        },
+      }),
+    };
+
+    const service = new Service(repo as any);
+
+    expect.assertions(1);
+
+    try {
+      await service.findAddressByIp('0.0.0.0');
+    } catch (error: any) {
+      expect(error.type).toBe(Geo2LiteServiceError.NoCityFound);
+    }
+  });
+
+  it('throws when no country is found', async () => {
+    const mockedCity = 'Everywhere, no where..';
+    const mockedPostal = 'Postman will have a hard time finding it';
+    const mockedAccuracyRadius = Number.POSITIVE_INFINITY;
+    const mockedTimeZone = 'Just another dimension here travels';
+
+    const repo = {
+      getAddressByIp: jest.fn().mockResolvedValueOnce({
+        city: mockedCity,
+        postal: mockedPostal,
+        location: {
+          accuracyRadius: mockedAccuracyRadius,
+          timeZone: mockedTimeZone,
+        },
+      }),
+    };
+
+    const service = new Service(repo as any);
+
+    expect.assertions(1);
+
+    try {
+      await service.findAddressByIp('0.0.0.0');
+    } catch (error: any) {
+      expect(error.type).toBe(Geo2LiteServiceError.NoCountryFound);
+    }
+  });
+
+  it('throws when no postal is found', async () => {
+    const mockedCity = 'Everywhere, no where..';
+    const mockedCountry = 'There are no countries here';
+    const mockedAccuracyRadius = Number.POSITIVE_INFINITY;
+    const mockedTimeZone = 'Just another dimension here travels';
+
+    const repo = {
+      getAddressByIp: jest.fn().mockResolvedValueOnce({
+        city: mockedCity,
+        country: mockedCountry,
+        location: {
+          accuracyRadius: mockedAccuracyRadius,
+          timeZone: mockedTimeZone,
+        },
+      }),
+    };
+
+    const service = new Service(repo as any);
+
+    expect.assertions(1);
+
+    try {
+      await service.findAddressByIp('0.0.0.0');
+    } catch (error: any) {
+      expect(error.type).toBe(Geo2LiteServiceError.NoPostalFound);
+    }
+  });
+
+  it('throws when no accuracy location is found', async () => {
+    const mockedCity = 'Everywhere, no where..';
+    const mockedCountry = 'There are no countries here';
+    const mockedPostal = 'Postman will have a hard time finding it';
+
+    const repo = {
+      getAddressByIp: jest.fn().mockResolvedValueOnce({
+        city: mockedCity,
+        country: mockedCountry,
+        postal: mockedPostal,
+      }),
+    };
+
+    const service = new Service(repo as any);
+
+    expect.assertions(1);
+
+    try {
+      await service.findAddressByIp('0.0.0.0');
+    } catch (error: any) {
+      expect(error.type).toBe(Geo2LiteServiceError.NoLocationFound);
+    }
+  });
+
+  it('throws when no timezone is found', async () => {
+    const mockedCity = 'Everywhere, no where..';
+    const mockedCountry = 'There are no countries here';
+    const mockedPostal = 'Postman will have a hard time finding it';
+    const mockedAccuracyRadius = Number.POSITIVE_INFINITY;
+
+    const repo = {
+      getAddressByIp: jest.fn().mockResolvedValueOnce({
+        city: mockedCity,
+        country: mockedCountry,
+        postal: mockedPostal,
+        location: {
+          accuracyRadius: mockedAccuracyRadius,
+        },
+      }),
+    };
+
+    const service = new Service(repo as any);
+
+    expect.assertions(1);
+
+    try {
+      await service.findAddressByIp('0.0.0.0');
+    } catch (error: any) {
+      expect(error.type).toBe(Geo2LiteServiceError.NoTimeZoneFound);
+    }
+  });
 });
