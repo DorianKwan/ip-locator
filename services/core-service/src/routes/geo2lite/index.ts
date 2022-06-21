7f456ad3c3f4742b11f4617cfb56f8988b58665f
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-06-20 23:41:39 -0600

    refactor: pass geoip2 account id to repo

--- services/core-service/src/routes/geo2lite/index.ts
+++ services/core-service/src/routes/geo2lite/index.ts
@@ -2,8 +2,8 @@ import Repo from './repo';
 import Service from './service';
 import Controller from './controller';
 
-export default (geo2LiteApiKey: string) => {
-  const repo = new Repo(geo2LiteApiKey);
+export default (geo2LiteApiKey: string, geo2LiteApiAccountId: string) => {
+  const repo = new Repo(geo2LiteApiKey, geo2LiteApiAccountId);
   const service = new Service(repo);
   const controller = Controller(service);
   return controller.routes();
